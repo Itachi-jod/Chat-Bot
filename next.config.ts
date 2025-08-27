a637a13036eaca46df95e188be6a4cf7a2437d23
Denesh Denesh <ddenesh072@gmail.com>
2025-08-27 15:36:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.pinimg.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'p16-sign-sg.tiktokcdn.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
